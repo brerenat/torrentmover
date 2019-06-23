a170d30743757f734efc31d8d1f274412ed1bdf8
Ginge117 <nathan-bf@live.co.uk>
2019-06-23 20:35:00 +0200

    Update to add FTP option

--- src/main/java/com/home/torrentmover/routes/file/FTPFileMoverProcessor.java
+++ src/main/java/com/home/torrentmover/routes/file/FTPFileMoverProcessor.java
@@ -1,33 +1,27 @@
 package com.home.torrentmover.routes.file;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FilenameFilter;
-import java.util.Arrays;
-import java.util.Date;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
-import javax.persistence.EntityManager;
-import javax.persistence.NoResultException;
 
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 import org.apache.camel.component.file.GenericFile;
+import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPClientConfig;
+import org.apache.commons.net.ftp.FTPFile;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.home.torrentmover.SpringStart;
-import com.home.torrentmover.model.FileType;
-import com.home.torrentmover.model.ProcessedFile;
 
-public class FileMoverProcessor implements Processor {
+public class FTPFileMoverProcessor implements Processor {
 
 	private static final String EPISODEREGEX = "[Ss]\\d{1,}[Ee]\\d{1,}";
 	private static final String MOVIEREGEX = "( \\d{4} )";
-	private static final Logger LOG = LoggerFactory.getLogger(FileMoverProcessor.class);
-
-	private static final String MOVIE = "Movie";
-	private static final String SERIES = "Series";
+	private static final Logger LOG = LoggerFactory.getLogger(FTPFileMoverProcessor.class);
 
 	private static final Pattern EPISODEPATTERN = Pattern.compile(EPISODEREGEX);
 	private static final Pattern MOVIEPATTERN = Pattern.compile(MOVIEREGEX);
@@ -35,7 +29,7 @@ public class FileMoverProcessor implements Processor {
 	private final String series;
 	private final String source;
 
-	public FileMoverProcessor() {
+	public FTPFileMoverProcessor() {
 		super();
 		this.movies = SpringStart.prop.getProperty("movies");
 		this.series = SpringStart.prop.getProperty("series");
@@ -56,34 +50,44 @@ public class FileMoverProcessor implements Processor {
 		final Matcher nameMatcher = EPISODEPATTERN.matcher(body.getFileNameOnly());
 		final String ext = body.getFileNameOnly().substring(body.getFileNameOnly().lastIndexOf('.'));
 		final String fileName = body.getFileNameOnly().substring(0, body.getFileNameOnly().lastIndexOf('.'));
-		File destination = null;
-		final String fileTypeStr;
+		String destination = null;
+		
+		final FTPClient ftp = new FTPClient();
+		final FTPClientConfig config = new FTPClientConfig();
+		config.setDefaultDateFormatStr("dd-MM-yyyy hh:mm:ss");
+
+		ftp.configure(config);
+		
+		ftp.connect(SpringStart.prop.getProperty("ftp.host"));
+		ftp.login(SpringStart.prop.getProperty("ftp.user"), SpringStart.prop.getProperty("ftp.password"));
+		
 		if (nameMatcher.find()) {
-			fileTypeStr = SERIES;
 			String seriesName = fileName.split(EPISODEREGEX)[0].replace('.', ' ').replace('_', ' ')
 					.replaceAll("\\[(.*?)\\]", "").replaceAll("\\((.*?)\\)", "").trim();
-			final File seriesDir = new File(series);
+			
+			final FTPFile[] seriesDirs = ftp.listDirectories(series);
 			LOG.info("Series Name :" + seriesName);
-			File seriesFolderFound = null;
-			for (final File dir : seriesDir.listFiles()) {
-				if (dir.isDirectory()
-						&& (dir.getName().equalsIgnoreCase(seriesName) || dir.getName().contains(seriesName))) {
+			FTPFile seriesFolderFound = null;
+			for (final FTPFile dir : seriesDirs) {
+				if (dir.getName().equalsIgnoreCase(seriesName)) {
 					seriesFolderFound = dir;
 					seriesName = dir.getName();
 				}
 			}
 			if (seriesFolderFound == null) {
-				final File newDir = new File(series + seriesName);
-				newDir.mkdir();
-				destination = new File(
-						newDir.getAbsolutePath() + File.pathSeparatorChar + seriesName + " " + nameMatcher.group().toUpperCase() + ext);
+				
+				LOG.info("Series :" + series + seriesName);
+				
+				ftp.makeDirectory(series + seriesName);
+				
+				destination = series + seriesName + "/" + seriesName + " " + nameMatcher.group().toUpperCase() + ext;
 			} else {
-				LOG.info("Found Series Folder");
-				destination = new File(seriesFolderFound.getAbsolutePath() + File.pathSeparatorChar + seriesName + " "
-						+ nameMatcher.group().toUpperCase() + ext);
+				seriesName = seriesName.substring(0, 1).toUpperCase() + seriesName.substring(1);
+				LOG.info("Found Series Folder :" + seriesName);
+				destination = series + "/" + seriesFolderFound.getName() + "/" + seriesName + " "
+						+ nameMatcher.group().toUpperCase() + ext;
 			}
 		} else {
-			fileTypeStr = MOVIE;
 			String newFileName = fileName.replace('.', ' ').replace('_', ' ').replaceAll("\\[(.*?)\\]", "")
 					.replaceAll("\\((.*?)\\)", "");
 			final Matcher matches = MOVIEPATTERN.matcher(newFileName);
@@ -92,10 +96,15 @@ public class FileMoverProcessor implements Processor {
 				newFileName = (newFileName.split(MOVIEREGEX)[0] + matches.group()).trim();
 				LOG.info("New File Name :" + newFileName);
 			}
-			destination = new File(movies + newFileName + ext);
+			destination = movies + newFileName + ext;
 		}
+		try (final FileInputStream fis = new FileInputStream(source)){
 		
-		source.renameTo(destination);
+			ftp.storeFile(destination, fis);
+		
+		} catch (final Exception e) {
+			e.printStackTrace();
+		}
 		
 		final File parentDir = source.getParentFile();
 		LOG.info("Parent Dir :" + parentDir.getAbsoluteFile());
@@ -108,46 +117,7 @@ public class FileMoverProcessor implements Processor {
 		}
 		
 		LOG.info("Old File Name :" + source.getAbsolutePath());
-		LOG.info("New File Name :" + destination.getAbsoluteFile());
-
-		final EntityManager em = SpringStart.getEntityManager();
-
-		if (em != null) {
-			em.getTransaction().begin();
-
-			FileType fileType;
-			try {
-				fileType = FileType.findWithName(em, fileTypeStr);
-			} catch (NoResultException e) {
-				LOG.warn("No Existing File Type :" + fileTypeStr);
-				fileType = new FileType();
-				fileType.setType(fileTypeStr);
-				em.persist(fileType);
-			}
-
-			ProcessedFile procFile;
-			try {
-				procFile = ProcessedFile.findWithName(em, destination.getAbsolutePath());
-			} catch (NoResultException e) {
-				LOG.warn("No Existing File with Name :" + destination.getAbsolutePath());
-				procFile = new ProcessedFile();
-				em.persist(fileType);
-				procFile.setFileName(destination.getAbsolutePath());
-			}
-			procFile.setDateProcessed(new Date());
-			procFile.setFileType(fileType);
-
-			em.persist(procFile);
-			em.getTransaction().commit();
-		}
-
-		String email = SpringStart.prop.getProperty("email.use");
-		if (email != null && "true".equals(email)) {
-			final String result = EmailMessageFormatter.getHTMLMessage(Arrays.asList("</br>From :<p>"
-					+ source.getAbsolutePath() + "</p></br>To :<p>" + destination.getAbsolutePath() + "</p>"));
-			exchange.getOut().setBody(result);
-			exchange.getOut().setHeader("myMail", email);
-		}
+		LOG.info("New File Name :" + destination);
 
 	}
 
@@ -159,7 +129,7 @@ public class FileMoverProcessor implements Processor {
 					LOG.info("Is not empty Dir");
 					emptyParent(file.listFiles());
 				}
-				if (!file.getAbsolutePath().equals(source) && !(file.getAbsolutePath() + File.pathSeparatorChar).equals(source)) {
+				if (!file.getAbsolutePath().equals(source) && !(file.getAbsolutePath() + "/").equals(source)) {
 					LOG.info("Deleting :" + file.getAbsoluteFile());
 					file.delete();
 				}
--- src/main/java/com/home/torrentmover/routes/file/FileMover.java
+++ src/main/java/com/home/torrentmover/routes/file/FileMover.java
@@ -5,6 +5,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
+import com.home.torrentmover.SpringStart;
+
 @Component
 public class FileMover extends RouteBuilder {
 
@@ -13,10 +15,15 @@ public class FileMover extends RouteBuilder {
 	@Override
 	public void configure() throws Exception {
 		LOG.info("Starting Configure");
-		from("file:{{source}}?recursive=true&noop=true&include={{file.formats}}").process(new FileMoverProcessor())
-				.choice().when(header("myMail").isEqualTo("true"))
-				.to("smtps://{{email.smtp}}:{{email.port}}?username={{email.sender}}&password={{email.password}}&from={{email.sender}}&to={{email.to}}&subject={{email.subject}}&contentType=text/html")
-				.otherwise().endChoice().end();
+		if (SpringStart.prop.getProperty("ftp.user") == null) {
+			from("file:{{source}}?recursive=true&noop=true&include={{file.formats}}").process(new FileMoverProcessor())
+					.choice().when(header("myMail").isEqualTo("true"))
+					.to("smtps://{{email.smtp}}:{{email.port}}?username={{email.sender}}&password={{email.password}}&from={{email.sender}}&to={{email.to}}&subject={{email.subject}}&contentType=text/html")
+					.otherwise().endChoice().end();
+		} else {
+			from("file:{{source}}?recursive=true&noop=true&include={{file.formats}}").process(new FTPFileMoverProcessor())
+			.end();
+		}
 	}
 
 }
